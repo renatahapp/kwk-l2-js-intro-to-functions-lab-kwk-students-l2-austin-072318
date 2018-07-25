4027b7d905861796c7b1c3dd0e81d212e7534c88
Renata Happle <renatahapp@gmail.com>
2018-07-25 18:38:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 // Write your code in this file!
+function getUKTime() {
+  
+  var result = new Date().getHours() + 5
+  return result
+}
 
+function discountTicket(x) { 
+let result = .8 * x 
+return result 
+}
 
+function tweetReply(fansName) { 
+  let result = "Hey " + fansName + ", thanks so much for the support! I really appreciate it!"
+  return result }
+  
