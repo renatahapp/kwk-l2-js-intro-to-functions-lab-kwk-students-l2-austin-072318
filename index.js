97087c873eddb9494854566eba655395c67582a7
Renata Happle <renatahapp@gmail.com>
2018-07-25 18:35:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ let result = .8 * x
 return result 
 }
 
-function tweetReply(x) { 
+function tweetReply(string) { 
   let result = "Hey " + x + ", thanks so much for the support! I really appreciate it! "
   return result }
   
